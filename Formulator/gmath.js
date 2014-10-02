c35b70d39b9acb818df295cf531ef899a0b14f78
gsung <samsung@gaviles.com>
2014-10-02 20:27:37 -0300

    jsfunction beta

---
+++ Formulator/gmath.js
@@ -0,0 +1,36 @@
+// initialization
+gmathBinds = [];
+
+$("[gmath]").each( function( i, e){
+	var gmathBind = $(e);
+	var attr = gmathBind.attr("gmath");
+	console.log(attr);
+	e.gmath = {type:""};
+	if( attr.charAt(0) == "=" ){
+		e.gmath.type = "function";
+		e.gmath.f = attr.slice(1);
+	}else{
+		gmathBind.on("keyup", function(){ gmathUpdate(); });
+		if( e.value == "" ){
+			e.value = 0;
+		}
+		e.gmath.type = "value";
+		e.gmath.index = attr;
+	}
+	gmathBinds.push(e);
+});
+
+function gmathUpdate(){
+		
+	for( var i in gmathBinds ){
+		if( gmathBinds[i].gmath.type == "value" ){
+			this[gmathBinds[i].gmath.index] = parseFloat( gmathBinds[i].value );
+		}
+	}
+	
+	for( var i in gmathBinds ){
+		if( gmathBinds[i].gmath.type == "function" ){
+			gmathBinds[i].innerText = eval(gmathBinds[i].gmath.f);
+		}
+	}
+}
